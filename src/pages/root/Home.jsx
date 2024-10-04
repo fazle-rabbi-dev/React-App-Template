d36410c4a7817850b41d6b58de73132233a96091
Fazle Rabbi <frabby983@gmail.com>
2024-10-04 17:52:29 +0600

    backup at 2024-10-04 05:52:29 PM

--- src/pages/root/Home.jsx
+++ src/pages/root/Home.jsx
@@ -1,14 +1,7 @@
 import { Navigate } from "react-router-dom";
-import { useUserStore } from "@/stores";
 import { Hero } from "@/components";
 
 export const Home = () => {
-  const isLoggedIn = useUserStore(state => state.isLoggedIn);
-  
-  if (isLoggedIn) {
-    return <Navigate to="/dashboard" />;
-  }
-  
   return (
     <>
       <Hero />
